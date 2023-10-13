754e44b7341244458e2fa7c420c0cf643440beb3
arch-spatula <shkim516@naver.com>
2023-10-13 22:06:16 +0900

    feat: 오늘 생성해야 하는 폴더와 파일 생성 #1

--- cmd/today.go
+++ cmd/today.go
@@ -5,10 +5,17 @@ package cmd
 
 import (
 	"fmt"
+	"os"
 	"time"
 
 	"github.com/spf13/cobra"
 )
+
+func check(e error) {
+	if e != nil {
+		panic(e)
+	}
+}
 
 // todayCmd represents the today command
 var todayCmd = &cobra.Command{
@@ -19,9 +26,21 @@ var todayCmd = &cobra.Command{
 이미 파일이 생성되어 있으면 실행하지 않습니다.
 `,
 	Run: func(cmd *cobra.Command, args []string) {
-		file := time.Now().Format("200601")
+		folder := time.Now().Format("200601")
 		markdown := time.Now().Format("20060102")
-		fmt.Println(file + "/" + markdown + ".md")
+
+		err := os.Mkdir(folder, 0755)
+		if err != nil {
+			fmt.Printf("Unable to write file: %v\n", err)
+		}
+
+		todayMarkdownFile := folder + "/" + markdown + ".md"
+
+		file, err := os.Create(todayMarkdownFile)
+		if err != nil {
+			fmt.Printf("Unable to write file: %v\n", err)
+		}
+		defer file.Close()
 	},
 }
 
