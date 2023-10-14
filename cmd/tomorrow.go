f8ebd678ccaef8bdd563c651e5508cf5d28b42ec
arch-spatula <shkim516@naver.com>
2023-10-14 19:22:25 +0900

    feat: 내일 파일 생성 명령을 추가 #1

--- cmd/tomorrow.go
+++ cmd/tomorrow.go
@@ -11,27 +11,28 @@ import (
 	"github.com/spf13/cobra"
 )
 
-// todayCmd represents the today command
-var todayCmd = &cobra.Command{
-	Use:   "today",
+// tomorrowCmd represents the tomorrow command
+var tomorrowCmd = &cobra.Command{
+	Use:   "tomorrow",
 	Short: "A brief description of your command",
-	Long: `오늘을 기준으로 TIL 문서를 자동생성합니다.
+	Long: `내일을 기준으로 TIL 문서를 자동생성합니다.
 
-이미 파일이 생성되어 있으면 실행하지 않습니다.
-`,
+	이미 파일이 생성되어 있으면 실행하지 않습니다.
+	`,
 	Run: func(cmd *cobra.Command, args []string) {
-		// 이번달 폴더 오늘 TIL 마크다운 파일이름 만들기
-		folder := time.Now().Format("0601")
-		markdown := time.Now().Format("060102")
+		// 이번달 폴더 내일 TIL 마크다운 파일이름 만들기
+		now := time.Now()
+		folder := now.Format("0601")
+		markdown := time.Date(now.Year(), now.Month(), now.Day()+1, 0, 0, 0, 0, time.UTC).Format("060102")
 
 		if err := os.Mkdir(folder, 0755); !os.IsExist(err) {
 			fmt.Println("이번달 폴더를 만들어두겠습니다.")
 		}
 
-		todayMarkdownFile := folder + "/TIL" + markdown + ".md"
+		tomorrowMarkdownFile := folder + "/TIL" + markdown + ".md"
 
-		if _, err := os.Stat(todayMarkdownFile); os.IsNotExist(err) {
-			todayFile, err := os.Create(todayMarkdownFile)
+		if _, err := os.Stat(tomorrowMarkdownFile); os.IsNotExist(err) {
+			todayFile, err := os.Create(tomorrowMarkdownFile)
 			if err != nil {
 				fmt.Printf("Unable to write file: %v\n", err)
 			}
@@ -46,24 +47,24 @@ var todayCmd = &cobra.Command{
 			// 오늘 TIL에 쓰기
 			fmt.Fprintln(todayFile, string(template))
 
-			fmt.Println(todayMarkdownFile, "을 만들어두겠습니다.")
+			fmt.Println(tomorrowMarkdownFile, "을 만들어두겠습니다.")
 		} else {
-			fmt.Println(todayMarkdownFile, "이 이미 만들어졌습니다.")
+			fmt.Println(tomorrowMarkdownFile, "이 이미 만들어졌습니다.")
 		}
 
 	},
 }
 
 func init() {
-	rootCmd.AddCommand(todayCmd)
+	rootCmd.AddCommand(tomorrowCmd)
 
 	// Here you will define your flags and configuration settings.
 
 	// Cobra supports Persistent Flags which will work for this command
 	// and all subcommands, e.g.:
-	// todayCmd.PersistentFlags().String("foo", "", "A help for foo")
+	// tomorrowCmd.PersistentFlags().String("foo", "", "A help for foo")
 
 	// Cobra supports local flags which will only run when this command
 	// is called directly, e.g.:
-	// todayCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	// tomorrowCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
