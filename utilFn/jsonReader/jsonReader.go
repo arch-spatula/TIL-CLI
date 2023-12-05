501d20cae2f026c1c2773740918ddef8eb8fbcb2
arch-spatula <shkim516@naver.com>
2023-12-05 23:07:27 +0900

    feat: json 읽기 관심사 분리 #13

--- utilFn/jsonReader/jsonReader.go
+++ utilFn/jsonReader/jsonReader.go
@@ -1,13 +1,47 @@
 package jsonReader
 
-import "fmt"
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"os"
+)
 
-type TILConfig struct{}
+type TILConfig struct {
+	CurrentProject         string `json:"current-project"`
+	CurrentProjectStartDay string `json:"current-project-start-day"`
+	ShowCurrentProject     bool   `json:"show-current-project"`
+	DaysWithoutAccidentDay string `json:"days-without-accident-day"`
+	DaysWithoutAccident    bool   `json:"days-without-accident"`
+	GratificationFormat    string `json:"gratification-format"`
+	GratificationDiary     bool   `json:"gratification-diary"`
+	Draft                  struct {
+		Today    string `json:"today"`
+		Tomorrow string `json:"tomorrow"`
+		Retro    string `json:"retro"`
+	} `json:"draft"`
+}
 
 func WriteJson() {
 	fmt.Println("hello writer json")
 }
 
-func ReadJson() {
-	fmt.Println("hello reader json")
+func ReadJson() TILConfig {
+	data, err := os.Open("til-config.json")
+	if err != nil {
+		panic(`til-config 파일이 없습니다.
+
+./TIL-CLI init 명령을 먼저 해주세요.`)
+	}
+	defer data.Close()
+
+	byteValue, _ := io.ReadAll(data)
+
+	var tilConfig TILConfig
+	err = json.Unmarshal(byteValue, &tilConfig)
+	if err != nil {
+		panic(err)
+	}
+
+	return tilConfig
 }
