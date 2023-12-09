ddaeac677a2847266a9384408a7f90424eff11cf
arch-spatula <shkim516@naver.com>
2023-12-09 14:47:31 +0900

    feat: 날씨 확인 기능 추가 #14

---
+++ cmd/experimental.go
@@ -0,0 +1,41 @@
+/*
+Copyright © 2023 NAME HERE <EMAIL ADDRESS>
+*/
+package cmd
+
+import (
+	"fmt"
+
+	"github.com/arch-spatula/TIL-CLI/utilFn/weather"
+	"github.com/spf13/cobra"
+)
+
+// experimentalCmd represents the experimental command
+var experimentalCmd = &cobra.Command{
+	Use:   "experimental",
+	Short: "정식 기능이 아니고 실험적인 명령입니다.",
+	Long: `정식 기능이 아니고 실험적인 명령입니다.
+
+./TIL-CLI experimental
+`,
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("experimental called")
+		for i, HdayFcast := range weather.ReadWeather().HdayFcastList {
+			fmt.Println(i, HdayFcast.AplYmd, HdayFcast.FullAreaName, HdayFcast.AmWetrTxt, HdayFcast.PmWetrTxt)
+		}
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(experimentalCmd)
+
+	// Here you will define your flags and configuration settings.
+
+	// Cobra supports Persistent Flags which will work for this command
+	// and all subcommands, e.g.:
+	// experimentalCmd.PersistentFlags().String("foo", "", "A help for foo")
+
+	// Cobra supports local flags which will only run when this command
+	// is called directly, e.g.:
+	// experimentalCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
